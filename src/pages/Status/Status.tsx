d99c8c23ebf5f3bd5006d140bf138067b95f2bbe
Paulo Moutinho <paulo_vicali@icloud.com>
2023-02-21 22:05:12 -0300

    feat: Captura dados do formulário

--- src/pages/Status/Status.tsx
+++ src/pages/Status/Status.tsx
@@ -1,24 +1,36 @@
+import { useState, FormEvent } from "react";
+import "./Status.css"
 import { Header } from "../../components/Header";
 import { Separator } from "../../components/Separator";
 import { Form } from "../../components/Form";
 import { Tweet } from "../../components/Tweet";
-import "./Status.css"
-
-const answers = ["Concordo...", "Faz sentido!", "Parabéns pelo progresso"]
 
 export function Status() {
+  const [ newAnswer, setNewAnswer ] = useState("");
+  const [ answers, setAnswers ] = useState<string[]>(["Corcordo...", "Depende da situação"]);
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault();
+
+    setAnswers([...answers, newAnswer]);
+    setNewAnswer("");
+  }
+
   return (
     <main className="status">
       <Header title="Tweet" />
       <Tweet content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis soluta ex, nesciunt mollitia a ducimus laborum iste repellat voluptas provident suscipit vitae autem sunt reiciendis libero ipsam? Incidunt, quae!"/>
       <Separator />
       <Form
+        onSubmit={createNewAnswer}
+        onChange={(event) => setNewAnswer(event.target.value)}
+        value={newAnswer}
         className="answer-tweet-form"
         placeholder="Tweet your answer"
         text="Answer"
       />
       {answers.map((answer, index) => {
-        return <Tweet key={index} content={answer}/>
+        return <Tweet key={index} content={answer} />;
       })}
     </main>
   );
